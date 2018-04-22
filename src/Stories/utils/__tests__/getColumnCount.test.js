a227cddeebed146f1305d58d754a29a55141a902
johnwatkins0 <johnwatkins0@gmail.com>
2018-04-21 21:44:03 -0400

    Modify test

--- src/Stories/utils/__tests__/getColumnCount.test.js
+++ src/Stories/utils/__tests__/getColumnCount.test.js
@@ -1,12 +1,12 @@
 import { getColumnCount } from '..';
 
 test('getColumnCount function', () => {
-  global.document.defaultView.clientWidth = 999;
+  window.innerWidth = 999;
   expect(getColumnCount(global.document.defaultView)).toBe(3);
 
-  global.document.defaultView.clientWidth = 750;
+  window.innerWidth = 750;
   expect(getColumnCount(global.document.defaultView)).toBe(2);
 
-  global.document.defaultView.clientWidth = 550;
+  window.innerWidth = 550;
   expect(getColumnCount(global.document.defaultView)).toBe(1);
 });
